8dbbfa6b8b6f9de504a743e5001942cbf2606b8f
CozyDoomer <chr.unterrainer@gmail.com>
2020-02-27 21:11:52 +0100

    use alias (this seems pointless :) )

--- task2.hs
+++ task2.hs
@@ -1,12 +1,12 @@
 type Nat0 = Int
 
-is_power2_iteration :: Int -> Int -> Bool
+is_power2_iteration :: Nat0 -> Nat0 -> Bool
 is_power2_iteration num power2
  | power2 < num = is_power2_iteration num (power2 * 2) 
  | power2 == num = True
  | otherwise = False
 
-is_power2 :: Int -> Bool
+is_power2 :: Nat0 -> Bool
 is_power2 x = is_power2_iteration x 1
 
 is_reversed_power2 :: Nat0 -> Bool
