82f417792a09e691879a5543c2930b0bd1004373
CozyDoomer <chr.unterrainer@gmail.com>
2020-03-02 22:04:40 +0100

    implement prime sieve instead of using Data.Numbers.Primes

--- task4.hs
+++ task4.hs
@@ -1,10 +1,22 @@
-import Data.Numbers.Primes
+sieve :: [Integer] -> [Integer]
+sieve (x:xs) = x : sieve [y | y <- xs, mod y x > 0]
 
-is_palindrom_prime :: Int -> Bool
-is_palindrom_prime n = (isPrime . read . reverse . show $ n) &&
-                 isPrime n 
+primes :: [Integer]
+primes = sieve [2..]
 
-double_prime :: (Int,Int) -> [Int]
+is_prime_iter :: Integer -> [Integer] -> Bool
+is_prime_iter n (x:xs)
+    | x > n = False
+    | x == n = True
+    | otherwise = is_prime_iter n xs
+
+is_prime :: Integer -> Bool
+is_prime n = is_prime_iter n primes
+
+is_palindrom_prime :: Integer -> Bool
+is_palindrom_prime n = (is_prime . read . reverse . show $ n) && is_prime n 
+
+double_prime :: (Integer,Integer) -> [Integer]
 double_prime (n1, n2)
     | n1 < n2 = filter is_palindrom_prime [n1 .. n2]
     | otherwise = filter is_palindrom_prime [n2 .. n1]
