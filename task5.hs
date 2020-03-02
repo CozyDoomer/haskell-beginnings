f1160e64d9a8e3c522201e8415fec6dc87b10246
CozyDoomer <chr.unterrainer@gmail.com>
2020-03-03 00:53:06 +0100

    refactor unit tests for task5.sh

--- task5.hs
+++ task5.hs
@@ -1,34 +1,29 @@
+import Data.List (intercalate)
 import Test.HUnit
 
-sum_proper_divisor :: Int -> Int
-sum_proper_divisor x = sum [ (i) | i <- [1 .. (x - 1)], mod x i == 0 ]
+sum_divisor :: Int -> Int
+sum_divisor x = sum [ (i) | i <- [1 .. (x - 1)], mod x i == 0 ]
 
-proper_divisor_sequence_rec :: Int -> [Int] -> [Int]
-proper_divisor_sequence_rec 1 result = 1:result
-proper_divisor_sequence_rec n result
+divisor_seq_rec :: Int -> [Int] -> [Int]
+divisor_seq_rec 1 result = 1:result
+divisor_seq_rec n result
     | n `elem` result = result
-    | otherwise = proper_divisor_sequence_rec (sum_proper_divisor n) (n:result)
+    | otherwise = divisor_seq_rec (sum_divisor n) (n:result)
 
-proper_divisor_sequence :: Int -> [Int]
-proper_divisor_sequence n = reverse $ proper_divisor_sequence_rec n []
+divisor_seq :: Int -> [Int]
+divisor_seq n = reverse $ divisor_seq_rec n []
 
--- HUnit Tests
--- 'runTestTT tests' to use them
 
-test1 = TestCase (assertEqual "func 3,"     [3,1]                           (proper_divisor_sequence 3))
-test2 = TestCase (assertEqual "func 4,"     [4,3,1]                         (proper_divisor_sequence 4))
-test3 = TestCase (assertEqual "func 6,"     [6]                             (proper_divisor_sequence 6))
-test4 = TestCase (assertEqual "func 10,"    [10,8,7,1]                      (proper_divisor_sequence 10))
-test5 = TestCase (assertEqual "func 12,"    [12,16,15,9,4,3,1]              (proper_divisor_sequence 12))
-test6 = TestCase (assertEqual "func 220,"   [220,284]                       (proper_divisor_sequence 220))
-test7 = TestCase (assertEqual "func 284,"   [284,220]                       (proper_divisor_sequence 284))
-test8 = TestCase (assertEqual "func 12496," [12496,14288,15472,14536,14264] (proper_divisor_sequence 12496))
+-- HUnit Tests: 'runTestTT tests' to use them
 
-tests = TestList [TestLabel "test1" test1, 
-                  TestLabel "test2" test2, 
-                  TestLabel "test3" test3, 
-                  TestLabel "test4" test4, 
-                  TestLabel "test5" test5, 
-                  TestLabel "test6" test6, 
-                  TestLabel "test7" test7, 
-                  TestLabel "test8" test8]
+equal_test :: Int -> [Int] -> Test
+equal_test arg result = TestCase (assertEqual ("divisor_seq " ++ show arg) result (divisor_seq arg))
+
+tests = TestList [equal_test     3 [3,1],
+                  equal_test     4 [4,3,1],
+                  equal_test     6 [6],
+                  equal_test    10 [10,8,7,1],
+                  equal_test    12 [12,16,15,9,4,3,1],
+                  equal_test   220 [220,284],
+                  equal_test   284 [284,220],
+                  equal_test 12496 [12496,14288,15472,14536,14264]]
